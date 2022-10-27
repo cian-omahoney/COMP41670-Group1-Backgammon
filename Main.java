7a93ed4efa1608673d0d537152b91db2b1860e58
cian-omahoney <cian.omahoney@ucdconnnect.ie>
2022-10-27 16:19:21 +0100

    Test change to main.

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
 		CommandType command = CommandType.QUIT;
 		System.out.print(command.toString());
 		System.out.println("Merge Test");
+		System.out.println("THis is a tst for ciaran. Written by cian");
 	}
 }
