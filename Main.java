f96997a803058c67c12931fa65e7f9e149001205
TangentSplash <68692742+TangentSplash@users.noreply.github.com>
2022-10-27 16:19:14 +0100

    Merge Conflict test

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ public class Main {
 		Backgammon backgammon = new Backgammon();
 		CommandType command = CommandType.QUIT;
 		System.out.print(command.toString());
-		System.out.println("Merge Test");
+		System.out.println("Conflicting merge test... FIXME!");
 	}
 }
