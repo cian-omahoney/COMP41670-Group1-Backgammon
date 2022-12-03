5117482b091e9ae3045ebd983cda70291065bff9
cian-omahoney <cian.omahoney@ucdconnnect.ie>
2022-12-03 16:05:13 +0000

    Add comments.

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Backgammon"},{"p":"<Unnamed>","l":"Bar"},{"p":"<Unnamed>","l":"Command"},{"p":"<Unnamed>","l":"Dice"},{"p":"<Unnamed>","l":"DoublingCube"},{"p":"<Unnamed>","l":"Game"},{"p":"<Unnamed>","l":"Match"},{"p":"<Unnamed>","l":"Point"}];updateSearchResults();
